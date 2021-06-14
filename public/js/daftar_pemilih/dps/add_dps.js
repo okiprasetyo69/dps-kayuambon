a71f0dfa27c6b3cf0d517366836de069c44d6532
okiprasetyo69 <oki.prasetyo45@gmail.com>
2021-06-14 17:01:26 +0700

    ajax import file

--- public/js/daftar_pemilih/dps/add_dps.js
+++ public/js/daftar_pemilih/dps/add_dps.js
@@ -40,6 +40,38 @@ $(document).ready(function () {
 
     //Import file
     $("body").on("submit", "#frm-import-file", function(e){
-        console.log('Masuk')
+        //console.log('Masuk')
+        $.ajax({
+            type: "POST",
+            url: "/api/dps/import",
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            beforeSend: function () {
+                $("#btn-save").attr("disabled", true);
+            },
+            success: function (response) {
+                //console.log(response);
+                document.getElementById("frm-add-dps").reset();
+                
+                $.confirm({
+                    title: "Message ",
+                    content: "Data berhasil di import !",
+                    buttons: {
+                        somethingElse: {
+                            text: "Ok",
+                            btnClass: "btn-success",
+                            keys: ["enter", "shift"],
+                            action: function () {
+                                tableDps.ajax.reload();
+                            },
+                        },
+                    },
+                });
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                $("#btn-save").attr("disabled", false);
+            },
+        });
     });
 });

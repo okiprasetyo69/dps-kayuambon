54e9b3cee7f57f1c108b386d98d38f5432940c18
okiprasetyo69 <oki.prasetyo45@gmail.com>
2021-06-17 16:14:46 +0700

    refactor home page

--- public/js/daftar_pemilih/dps/home.js
+++ public/js/daftar_pemilih/dps/home.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
     //console.log('test')
+    $('#dps-card').hide();
+    $('#btn-reset').hide();
     $('#btn-reset').on('click', function(e){
         e.preventDefault
-        $('#rowData').html("");
         $('#nik').html("");
         $('#nik').val("");
+        $('#nik').focus(); 
+        $('#dps-card').hide();
+        $('#btn-reset').hide();
+        clearElement();
     });
     $("body").on("submit", "#frm-filter-dps", function (e) {
         e.preventDefault();
@@ -34,14 +39,59 @@ $(document).ready(function () {
                         },
                     });
                 } else {
-                    let row_data = '';
-                    //let keterangan = response.keterangan == null ? 0 : response.keterangan
-                    row_data= '<tr class="text-center"><td> '+response.nkk+' </td><td>'+response.nik+'</td><td>'+response.nama+'</td><td>'+response.tempat_lahir+'</td><td>'+response.tgl_lahir+'</td><td>'+response.kawin+'</td><td>'+response.alamat+'</td><td>'+response.rt+'</td><td>'+response.rw+'</td><td>'+response.difabel+'</td><td>'+ response.keterangan +'</td><td>'+response.sumberdata+'</td><td>'+response.tps+'</td> </tr>';
-                    $('#rowData').html("");
-                    $('#rowData').append(row_data);
+                    //let row_data = '';
+                    let jenis_kelamin = response.jenis_kelamin;
+                    //let male =  "{!! asset('images/user_male.jpg') !!}";
+                    //let female =  "{!! asset('images/user_female.png') !!}";
+                    let male =  "http://localhost:8000/images/user_male.jpg";
+                    let female =  "http://localhost:8000/images/user_female.png";
+                  
+                    $('#dps-card').show();
+                    $('#btn-reset').show();
+                    
+                    clearElement();
+                    //row_data= '<tr class="text-center"><td> '+response.nkk+' </td><td>'+response.nik+'</td><td>'+response.nama+'</td><td>'+response.tempat_lahir+'</td><td>'+response.tgl_lahir+'</td><td>'+response.kawin+'</td><td>'+response.alamat+'</td><td>'+response.rt+'</td><td>'+response.rw+'</td><td>'+response.difabel+'</td><td>'+ response.keterangan +'</td><td>'+response.sumberdata+'</td><td>'+response.tps+'</td> </tr>';
+                    if(jenis_kelamin == "L"){
+                        $("#img-dps").attr('src', male)
+                    } else {
+                        $('#img-dps').attr('src', female)
+                    }   
+
+                    $('#nama').html('Nama : ' +response.nama);
+                    $('#alamat').html('Alamat : ' +response.alamat);
+                    $('#nkk').html('NKK : ' + response.nama);
+                    $('#nik-card').html('NIK : ' +response.nik);
+                    $('#jenis_kelamin').html('Jenis Kelamin : ' + response.jenis_kelamin);
+                    $('#kawin').html('Kawin : ' + response.kawin);
+                    $('#tempat_lahir').html('Tempat Lahir : ' + response.tempat_lahir);
+                    $('#tgl_lahir').html('Tanggal Lahir : ' +response.tgl_lahir);
+                    $('#rt').html('RT : ' + response.rt);
+                    $('#rw').html('RW : ' + response.rw);
+                    $('#difabel').html('Difabel : ' + response.difabel);
+                    $('#keterangan').html('Keterangan : ' + response.keterangan);
+                    $('#sumberdata').html('Sumber Data : ' + response.sumberdata);
+                    $('#tps').html('TPS : ' + response.tps);
                 }              
             }
         });
         
     });
 });
+
+function clearElement(){
+    $('#nama').html("");
+    $('#alamat').html("");
+    $('#nkk').html("");
+    $('#nik-card').html("");
+    $('#nama').html("");
+    $('#jenis_kelamin').html("");
+    $('#kawin').html("");
+    $('#tempat_lahir').html("");
+    $('#tgl_lahir').html("");
+    $('#rt').html("");
+    $('#rw').html("");
+    $('#difabel').html("");
+    $('#keterangan').html("");
+    $('#sumberdata').html("");
+    $('#tps').html("");
+}
--- resources/views/dsp-home.blade.php
+++ resources/views/dsp-home.blade.php
@@ -3,7 +3,7 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,shrink-to-fit=no">
     <title>Desa Kayuambon</title>
     <link href="{{ asset('') }}css/styles.css" rel="stylesheet" />
     <link href="{{ asset('') }}css/custom.css" rel="stylesheet" />
@@ -34,12 +34,132 @@
     @yield('custom-css')
   </head>
   <body>
-      <div class="container">
+      <header> 
         <div class="row">
-            <div class="col text-center">
-                <h1>INFO DAFTAR PEMILH (DPS) PILKADES KAYUAMBON</h1>
-            </div>
+          <div class="col">
+                <!--Navbar-->
+                <nav class="navbar navbar-dark bg-primary">
+                  <!-- Navbar brand -->
+                  <img src="{{ asset('') }}assets/img/bandung_barat.png" style="height:70px; width:70px;"/>
+                  <h3 class="text-wrap" style="color: aliceblue;">INFO DAFTAR PEMILH (DPS) PILKADES KAYUAMBON</h3>
+                  <!-- Collapse button -->
+                  <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+                    aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
+                        class="fas fa-bars fa-1x"></i></span></button>
+                  <!-- Collapsible content -->
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+                    <!-- Links -->
+                   
+                    <ul class="navbar-nav mr-auto">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                      </li>
+                      <!-- 
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                      </li>
+                      -->
+                    </ul>
+                    <!-- Links -->
+                  </div>
+                  <!-- Collapsible content -->
+                </nav>
+                <!--/.Navbar-->
+          </div>
         </div>
+      </header>
+      <div class="row mt-4">
+        <div class="container">
+          <form id="frm-filter-dps">  
+            @csrf
+            <div class="row">
+              <div class="col-md-4"></div>
+              <div class="col-md-4">
+                <input class="form-control" type="text" name="nik" id="nik" placeholder="Masukkan NIK penduduk" aria-label="Search" autofocus>
+              </div>
+              <div class="col-md-2">
+                <button class="btn btn-success btn-md"  type="submit">
+                  <i class="fas fa-search-minus">  </i> 
+                  Cari
+                </button>
+              </div>
+              <div class="col-md-2"></div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="row mt-4">
+          <div class="container" id="dps-card">
+              <div class="card">
+                    <img class="card-img-top img-fluid rounded mx-auto d-block" id="img-dps" style="height:100px; width:100px;">
+                    <div class="card-body">
+                      <h5 class="card-title text-center" id="nama"> </h5>
+                      <p class="card-text text-center" id="alamat"> </p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <li class="list-group-item" id="nkk"> </li>
+                        </div>
+                        <div class="col-md-6 pd-0">
+                          <li class="list-group-item" id="nik-card"></li>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <li class="list-group-item" id="jenis_kelamin"></li>
+                        </div>
+                        <div class="col-md-6 pd-0">
+                          <li class="list-group-item" id="kawin"></li>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <li class="list-group-item" id="tempat_lahir"></li>
+                        </div>
+                        <div class="col-md-6 pd-0">
+                          <li class="list-group-item" id="tgl_lahir"></li>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <li class="list-group-item" id="rt"> </li>
+                        </div>
+                        <div class="col-md-6 pd-0">
+                          <li class="list-group-item" id="rw"></li>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <li class="list-group-item" id="difabel"></li>
+                        </div>
+                        <div class="col-md-6 pd-0">
+                          <li class="list-group-item" id="keterangan"></li>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <li class="list-group-item" id="sumberdata"></li>
+                        </div>
+                        <div class="col-md-6 pd-0">
+                          <li class="list-group-item" id="tps"></li>
+                        </div>
+                      </div>
+                    </ul>
+                    <!-- 
+                    <div class="card-body">
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                    -->
+              </div>
+          </div>
+      </div>
+        <!-- 
+      <div class="container">
         <br>
         <form id="frm-filter-dps">  
           @csrf
@@ -57,9 +177,10 @@
         </div>
         </form>
         <br>
+      
         <div class="row">
           <div class="col" style="padding:0px;">
-            <table class="table table-striped table-border">
+            <table class="table table-striped table-border table-responsive">
               <thead>
                 <tr class="text-center">
                   <th>NKK</th>
@@ -83,14 +204,23 @@
             </table>
           </div>
         </div>
-
+        
         <div class="row">
           <div class="col">
             <button type="button" id="btn-reset" class="btn btn-secondary btn-sm float-right">Reset</button>
           </div>
         </div>
+      
       </div>
-     
+        -->
+      <div class="row mt-4">
+        <div class="container">
+          <div class="col" style="padding:0px;">
+            <button type="button" id="btn-reset" class="btn btn-secondary btn-md  float-left ">Reset</button>
+          </div>
+        </div>
+      </div>
+        
       <script src="{{ asset('') }}assets/plugins/bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
       <script src="{{ asset('') }}js/scripts.js"></script>
       <script src="/main.js"></script>
